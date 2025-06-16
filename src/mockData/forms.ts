34987f6f56da783f32b2f42ece990e9677f699cd
TehilaFried <49166882+TehilaFried@users.noreply.github.com>
2025-06-16 16:37:37 +0300

    change again  mook data

--- src/mockData/forms.ts
+++ src/mockData/forms.ts
@@ -181,4 +181,4 @@ const form10: TForm = {
 };
 
 
-export const myForms: TForm[] = [form1, form2, form3, form4, form5, form6, form7, form8, form9, form10];
+export const myForms: TForm[] = [form1, form6, form7, form8, form9, form10];
