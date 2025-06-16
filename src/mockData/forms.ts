12940593ba231bef58aafd7b96ed43239ec27c42
Tal Mekler <tal.mekler11@gmail.com>
2025-06-16 17:39:36 +0300

    update form type + update mock data

--- src/mockData/forms.ts
+++ src/mockData/forms.ts
@@ -1,25 +1,26 @@
 import type { TForm } from "../types/TForm";
 
-const form: TForm = {
+export const form: TForm = {
   id: "form-1",
   title: "Sample Form",
-  description: "This is a sample form for demonstration purposes.",
+  description: "לורם איפסום דולור סיט אמט, קונסקטורר לורם איפסום דולור סיט אמט, קונסקטורר ",
   createdAt: "2023-10-01T12:00:00Z",
   updatedAt: "2023-10-01T12:00:00Z",
   isPublished: true,
   author: {
     id: "user-1",
-    firstName: "John",
-    lastName: "Doe",
+    firstName: "טל",
+    lastName: "מקלר",
     email: "asd@gmail.com",
-    image: null, // URL to the user's image or null if not available
+    image: "https://picsum.photos/200", // URL to the user's image or null if not available
     createdAt: "2023-10-01T12:00:00Z",
     phoneNumber: null, // phone number or null if not available
   },
-  image: "",
-  categories: ["General", "Feedback"],
+  image: "https://picsum.photos/200",
+  categories: ["לוגיסטיקה", `אכ״א`, "דחיפות"],
   tags: ["sample", "demo", "form"],
   expirationDate: null,
+  timeToFill: "5 דקות", // estimated time to fill the form
 };
 
 export const myForms: TForm[] = [form, form, form, form, form, form, form, form, form, form];
