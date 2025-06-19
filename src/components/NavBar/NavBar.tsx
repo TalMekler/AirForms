606f5a80ddde02125b8db73d9dc82f95f846a2c0
Tal Mekler <tal.mekler11@gmail.com>
2025-06-19 17:15:47 +0300

    refactor navbar styling architecture

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,19 +6,17 @@ import CustomNavItem from "./CustomNavItem";
 
 const NavBar = () => {
   return (
-    <AppBar position="static" color="transparent" elevation={0}>
-      <Toolbar sx={{ justifyContent: "space-between" }}>
-        <Stack direction="row" spacing={2}>
-          <CustomNavItem label="דף הבית" MuiIcon={HomeOutlined} selected />
-          <CustomNavItem label="ניהול קבוצות" MuiIcon={GroupsOutlined} />
-          <CustomNavItem label="ניהול טופס" MuiIcon={EditNoteOutlined} />
-          <CustomNavItem label="אזור אישי" MuiIcon={BadgeOutlined} />
-        </Stack>
-        <Box component={Link} to={"/"}>
-          <img src={AppLogo} />
-        </Box>
-      </Toolbar>
-    </AppBar>
+    <Stack direction={"row"} alignItems={"center"} justifyContent={"space-between"}>
+      <Stack direction="row" spacing={2}>
+        <CustomNavItem label="דף הבית" MuiIcon={HomeOutlined} selected />
+        <CustomNavItem label="ניהול קבוצות" MuiIcon={GroupsOutlined} />
+        <CustomNavItem label="ניהול טופס" MuiIcon={EditNoteOutlined} />
+        <CustomNavItem label="אזור אישי" MuiIcon={BadgeOutlined} />
+      </Stack>
+      <Box component={Link} to={"/"}>
+        <img src={AppLogo} />
+      </Box>
+    </Stack>
   );
 };
 
