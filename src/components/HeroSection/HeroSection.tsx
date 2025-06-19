e519e8bfe0e9bef5f36fa3abac3c7abb46e53ab4
Tal Mekler <tal.mekler11@gmail.com>
2025-06-19 14:19:55 +0300

    refactor the HeroSection component

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,64 +1,38 @@
-import React from 'react';
-import { Box, Typography, Button, Grid } from '@mui/material';
-import StarIcon from '@mui/icons-material/Star';
+import React from "react";
+import { Box, Typography, Button, Grid, Stack } from "@mui/material";
+import HeroSectionImage from "../../assets/heroSectionImage.svg";
 
 const HeroSection: React.FC = () => {
-    return (
-        <Box
-            sx={{
-                px: 4,
-                py: 8,
-                backgroundColor: 'white',
-            }}
-        >
-            <Grid container spacing={50} alignItems="center">
-                <Grid >
-                    <Typography variant="h6" fontWeight="light" mb={1}>
-                        לא צריך לחפש ..
-                    </Typography>
+  return (
+    <Grid container spacing={4}>
+      <Grid size={{ xs: 12, md: 12, lg: 6 }} display={"flex"} justifyContent={{ xs: "center", lg: "start" }}>
+        <Stack gap={"60px"}>
+          <Box>
+            <Typography variant="h6" fontSize={77.5} fontFamily={"Karantina"}>
+              לא צריך לחפש ..
+            </Typography>
 
-                    <Typography
-                        variant="h3"
-                        fontWeight="bold"
-                        mb={3}
-                        sx={{ fontFamily: 'inherit', lineHeight: 1.2 }}
-                    >
-                        הכל במקום אחד!
-                    </Typography>
+            <Typography variant="h3" fontSize={147.5} fontFamily={"Karantina"}>
+              הכל במקום אחד!
+            </Typography>
+          </Box>
 
-                    <Typography variant="body1" color="text.secondary" mb={4} sx={{ maxWidth: 400 }}>
-                        הממשק הדיגיטלי החדש של הלל האוויר הוא קפיצת מדרגה בעולם הניהול והבקשות. כל הטפסים במקום אחד,
-                        עם חווית שימוש פשוטה, נוחה וידידותית מאי פעם. פחות בירוקרטיה, יותר יעילות – הטכנולוגיה שמעצימה אתכם!
-                    </Typography>
-
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        sx={{ bgcolor: '#0D6BA8', fontWeight: 'bold', borderRadius: "16px", px: 4, py: 1.5 }}
-                    >
-                        צרו טופס חדש
-                    </Button>
-                </Grid>
-                <Grid  >
-                    <Box
-                        component="img"
-                        src="https://picsum.photos/200"
-                        alt="Hero Illustration"
-                        sx={{ width: '100%', maxWidth: 400, mx: 'auto', display: 'block' }}
-                    />
-                    <StarIcon
-                        sx={{
-                            position: 'absolute',
-                            top: '25rem',
-                            right: '15rem',
-                            color: '#A6E1FA',
-                            fontSize: 60,
-                        }}
-                    />
-                </Grid>
-            </Grid>
-        </Box>
-    );
+          <Typography variant="body1" color="text.secondary" mb={4} sx={{ maxWidth: 400 }}>
+            הממשק הדיגיטלי החדש של הלל האוויר הוא קפיצת מדרגה בעולם הניהול והבקשות. כל הטפסים במקום אחד, עם חווית שימוש
+            פשוטה, נוחה וידידותית מאי פעם. פחות בירוקרטיה, יותר יעילות – הטכנולוגיה שמעצימה אתכם!
+          </Typography>
+          <Box>
+            <Button variant="contained" color="secondary" sx={{ px: "40px", borderRadius: 2, fontSize: 24 }}>
+              צרו טופס חדש
+            </Button>
+          </Box>
+        </Stack>
+      </Grid>
+      <Grid size={{ md: 12, lg: 6 }} display={"flex"} justifyContent={{ xs: "center", lg: "start" }}>
+        <img src={HeroSectionImage} width={"100%"} height={700} style={{ maxWidth: "570px" }} />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default HeroSection;
