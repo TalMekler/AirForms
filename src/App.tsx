213787953f60e65b7a669d308459274c769a12e3
Tal Mekler <tal.mekler11@gmail.com>
2025-06-19 17:15:30 +0300

    wrap pages in the Router with the Page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { Typography } from "@mui/material";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import HomePage from "./components/pages/HomePage/HomePage";
-
-
+import Page from "./components/Page/Page";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={
+            <Page>
+              <HomePage />
+            </Page>
+          }
+        />
         <Route path="*" element={<Typography>Page Not Found</Typography>} />
       </Routes>
     </Router>
