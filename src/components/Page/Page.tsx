01b64339d39e17ea20ba254d3b3370cc936e80f8
Tal Mekler <tal.mekler11@gmail.com>
2025-06-23 11:36:50 +0300

    add the Footer into the Page component

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,16 +1,20 @@
 import { Box } from "@mui/material";
 import React, { type PropsWithChildren } from "react";
 import NavBar from "../NavBar/NavBar";
+import Footer from "../Footer/Footer";
 
 type PageProps = {};
 const Page = ({ children }: PropsWithChildren<PageProps>) => {
   return (
-    <Box py={"2rem"} px={"7.5rem"} bgcolor={"#FAFAFA"} minHeight={"100vh"}>
-      <Box mb={"3.125rem"}>
-        <NavBar />
+    <>
+      <Box py={"2rem"} px={"7.5rem"} bgcolor={"#FAFAFA"} minHeight={"100vh"}>
+        <Box mb={"3.125rem"}>
+          <NavBar />
+        </Box>
+        {children}
       </Box>
-      {children}
-    </Box>
+      <Footer />
+    </>
   );
 };
 
