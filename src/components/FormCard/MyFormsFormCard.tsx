e933c70885f554fc9a98c8f5d70a4be252d52b65
Tal Mekler <tal.mekler11@gmail.com>
2025-06-23 17:55:44 +0300

    fix ui

--- src/components/FormCard/MyFormsFormCard.tsx
+++ src/components/FormCard/MyFormsFormCard.tsx
@@ -1,44 +1,44 @@
-import { Bookmark, IosShare } from "@mui/icons-material";
-import { Box, Chip, Stack, Typography } from "@mui/material";
-import type { TForm } from "../../types/TForm";
-import FormCard from "./FormCard";
+import { Bookmark, IosShare } from '@mui/icons-material';
+import { Box, Chip, Stack, Typography } from '@mui/material';
+import type { TForm } from '../../types/TForm';
+import FormCard from './FormCard';
 
 type MyFormsFormCardProps = {
-	form: TForm;
+  form: TForm;
 };
 const MyFormsFormCard = ({ form }: MyFormsFormCardProps) => {
-	return (
-		<>
-			<FormCard form={form} width={"100%"} height={"100%"} borderRadius={2} overflow={"hidden"} boxShadow={3} maxWidth={400}>
-				<Stack bgcolor={"white"} gap={"1.25rem"} p={"1.25rem"}>
-					<Stack direction={"row"} justifyContent={"space-between"}>
-						<Stack direction={"row"} alignItems={"center"} gap={"0.5625rem"}>
-							<Box>
-								<FormCard.Image width={32} height={32} />
-							</Box>
-							<FormCard.Title variant="h3" fontSize={20} fontWeight={600} numOfRows={1} />
-						</Stack>
-						<FormCard.IsPublishedChip />
-						<Stack direction={"row"} alignItems={"center"} gap={"0.5625rem"}>
-							<Stack direction={"row"} gap={"0.3125rem"}>
-								<Bookmark />
-								<IosShare sx={{ cursor: "pointer" }} />
-							</Stack>
-						</Stack>
-					</Stack>
-					<FormCard.Description fontSize={15} fontWeight={500} color="#727272" numOfRows={2} />
-					<Stack direction={"row"} alignItems={"center"} justifyContent={"space-between"}>
-						<Stack direction={"row"} gap={"0.3125rem"}>
-							<FormCard.Categories maxItemsToRender={3} renderItem={(st: string) => <Chip label={st} sx={{ bgcolor: "#def5fe" }} />} />
-						</Stack>
-						<Typography variant="body2" fontSize={15} color="#727272">
-							זמן מילוי כ-{form.timeToFill}
-						</Typography>
-					</Stack>
-				</Stack>
-			</FormCard>
-		</>
-	);
+  return (
+    <>
+      <FormCard form={form} width={'100%'} height={'100%'} borderRadius={2} overflow={'hidden'} boxShadow={3} maxWidth={400}>
+        <Stack bgcolor={'white'} gap={'1.25rem'} p={'1.25rem'} height={'100%'}>
+          <Stack direction={'row'} justifyContent={'space-between'}>
+            <Stack direction={'row'} alignItems={'center'} gap={'0.5625rem'}>
+              <Box>
+                <FormCard.Image width={32} height={32} />
+              </Box>
+              <FormCard.Title variant="h3" fontSize={20} fontWeight={600} numOfRows={1} />
+            </Stack>
+            <FormCard.IsPublishedChip />
+            <Stack direction={'row'} alignItems={'center'} gap={'0.5625rem'}>
+              <Stack direction={'row'} gap={'0.3125rem'}>
+                <Bookmark />
+                <IosShare sx={{ cursor: 'pointer' }} />
+              </Stack>
+            </Stack>
+          </Stack>
+          <FormCard.Description fontSize={15} fontWeight={500} color="#727272" numOfRows={2} />
+          <Stack direction={'row'} mt={'auto'} alignItems={'center'} justifyContent={'space-between'}>
+            <Stack direction={'row'} gap={'0.3125rem'}>
+              <FormCard.Categories maxItemsToRender={3} renderItem={(st: string) => <Chip label={st} sx={{ bgcolor: '#def5fe' }} />} />
+            </Stack>
+            <Typography variant="body2" fontSize={15} color="#727272">
+              זמן מילוי כ-{form.timeToFill}
+            </Typography>
+          </Stack>
+        </Stack>
+      </FormCard>
+    </>
+  );
 };
 
 export default MyFormsFormCard;
