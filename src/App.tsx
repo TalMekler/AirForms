30a322733aeb476107114560ba176e5911076726
Tal Mekler <tal.mekler11@gmail.com>
2025-06-23 17:47:00 +0300

    revert changes in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { Box, Typography } from "@mui/material";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import MyFormsFormCard from "./components/FormCard/MyFormsFormCard";
-import NewFormsFormCard from "./components/FormCard/NewFormsFormCard";
-import GenericStrip from "./components/GenericStripe";
-import Page from "./components/Page/Page";
-import HomePage from "./components/pages/HomePage/HomePage";
-import { myForms } from "./mockData/forms";
-import type { TForm } from "./types/TForm";
+import { Typography } from '@mui/material';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Page from './components/Page/Page';
+import HomePage from './components/pages/HomePage/HomePage';
 
 function App() {
   return (
@@ -29,12 +24,6 @@ function App() {
           }
         />
       </Routes>
-      <Box>
-        <GenericStrip<TForm> title="טפסים חדשים" items={myForms} maxItemsToShow={10} emptyMessage="אין טפסים להצגה" actionButtonText="לכל הטפסים" renderItem={(form) => <NewFormsFormCard form={form} />} />
-      </Box>
-      <Box mt={10}>
-        <GenericStrip<TForm> title="טפסים שיצרתי" items={myForms} maxItemsToShow={5} emptyMessage="אין טפסים להצגה" actionButtonText="לכל הטפסים" renderItem={(form) => <MyFormsFormCard form={form} />} />
-      </Box>
     </Router>
   );
 }
