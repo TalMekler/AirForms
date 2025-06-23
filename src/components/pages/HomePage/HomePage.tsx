d471a96da1dce755974791a4a7eefa0a65efafd8
Tal Mekler <tal.mekler11@gmail.com>
2025-06-23 17:55:25 +0300

    add stripes to the home page

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,14 +1,20 @@
-import { Search, Tune } from "@mui/icons-material";
-import { Box, IconButton, InputAdornment, Stack, TextField } from "@mui/material";
-import FilterCategories from "../../FilterCategories/FilterCategories";
-import { filterCategories } from "../../FilterCategories/utils/filterCategories";
-import HeroSection from "../../HeroSection/HeroSection";
+import { Search, Tune } from '@mui/icons-material';
+import { Box, IconButton, InputAdornment, Stack, TextField, Typography } from '@mui/material';
+import { kit1, myForms } from '../../../mockData/forms';
+import type { TForm } from '../../../types/TForm';
+import FilterCategories from '../../FilterCategories/FilterCategories';
+import { filterCategories } from '../../FilterCategories/utils/filterCategories';
+import MyFormsFormCard from '../../FormCard/MyFormsFormCard';
+import NewFormsFormCard from '../../FormCard/NewFormsFormCard';
+import HeroSection from '../../HeroSection/HeroSection';
+import HomePageCardsStripe from './HomePageCardsStripe';
+import type { TKit } from '../../../types/TKit';
 
 const HomePage = () => {
   return (
     <Box>
       <HeroSection />
-      <Stack mt={"2.5rem"} mb={"3.75rem"} width={"fit-content"} mx={"auto"} direction={"row"} gap={"1.25rem"} alignItems={"center"}>
+      <Stack mt={'2.5rem'} mb={'3.75rem'} width={'fit-content'} mx={'auto'} direction={'row'} gap={'1.25rem'} alignItems={'center'}>
         <TextField
           placeholder="חיפוש"
           InputProps={{
@@ -18,25 +24,22 @@ const HomePage = () => {
               </InputAdornment>
             ),
           }}
-          sx={{ width: 540, px: 2, py: 1 }}
+          sx={{ width: '33.75rem', px: 2, py: 1 }}
         />
         <Box>
-          <IconButton
-            sx={{
-              height: 60,
-              width: 60,
-              borderRadius: 2,
-              bgcolor: "#EDF5FE",
-              "&:hover": {
-                bgcolor: "#D0E4FB",
-              },
-            }}
-          >
+          <IconButton sx={{ height: '3.75rem', width: '3.75rem', borderRadius: 2, bgcolor: '#EDF5FE', '&:hover': { bgcolor: '#D0E4FB' } }}>
             <Tune />
           </IconButton>
         </Box>
       </Stack>
       <FilterCategories filterCategories={filterCategories} />
+
+      <Stack gap={'9.375rem'} mt={'7.5rem'}>
+        <HomePageCardsStripe title="טפסים חדשים" items={myForms.slice(0, 4) as TForm[]} renderItem={(form) => <NewFormsFormCard form={form as TForm} />} buttonText="לכל הטפסים" buttonOnClick={() => {}} />
+        <HomePageCardsStripe title="טפסים שיצרתי" items={myForms.slice(0, 8) as TForm[]} renderItem={(form) => <MyFormsFormCard form={form as TForm} />} buttonText="לכל היצירות" buttonOnClick={() => {}} />
+        <HomePageCardsStripe title="התיקיות שלי" items={[]} renderItem={(form) => <MyFormsFormCard form={form as TForm} />} buttonText="לכל התיקיות" buttonOnClick={() => {}} />
+        <HomePageCardsStripe title="מועדפים" items={[...myForms.slice(0, 4), kit1, kit1, kit1, kit1]} renderItem={(item: TForm | TKit) => ('forms' in item ? <Typography>TKit</Typography> : <MyFormsFormCard form={item} />)} buttonText="מועדפים נוספים" buttonOnClick={() => {}} />
+      </Stack>
     </Box>
   );
 };
