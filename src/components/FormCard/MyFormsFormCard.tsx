d4b08097b53f053461d939797d88fb12dee9511b
Tal Mekler <tal.mekler11@gmail.com>
2025-06-28 11:17:35 +0300

    fix style

--- src/components/FormCard/MyFormsFormCard.tsx
+++ src/components/FormCard/MyFormsFormCard.tsx
@@ -11,7 +11,7 @@ const MyFormsFormCard = ({ form, displayStatus }: MyFormsFormCardProps) => {
   return (
     <>
       <FormCard form={form} width={"100%"} height={"100%"} borderRadius={2} overflow={"hidden"} boxShadow={`0 1px 10px 0 #001C5533`} maxWidth={400}>
-        <Stack bgcolor={"white"} gap={"1.25rem"} p={"1.25rem"}>
+        <Stack bgcolor={"white"} gap={"1.25rem"} p={"1.25rem"} height={"100%"}>
           <Stack direction={"row"} justifyContent={"space-between"}>
             <Stack direction={"row"} alignItems={"center"} gap={"0.5625rem"}>
               <Box>
@@ -28,7 +28,7 @@ const MyFormsFormCard = ({ form, displayStatus }: MyFormsFormCardProps) => {
             </Stack>
           </Stack>
           <FormCard.Description fontSize={15} fontWeight={500} color="#727272" numOfRows={2} />
-          <Stack direction={"row"} alignItems={"center"} justifyContent={"space-between"}>
+          <Stack mt={"auto"} direction={"row"} alignItems={"center"} justifyContent={"space-between"}>
             <Stack direction={"row"} gap={"0.3125rem"}>
               <FormCard.Categories maxItemsToRender={3} renderItem={(st: string) => <Chip label={st} sx={{ bgcolor: "#def5fe" }} />} />
             </Stack>
